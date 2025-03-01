0fb5178ca84e1f486f9f119755376bbb3672e19f
Syamsul Alam <admin@alampintar.org>
2025-03-02 01:23:14 +0700

    Update Vite configuration to load environment variables for Pinata API keys

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { loadEnv } from 'vite'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-});
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.VITE_PINATA_API_KEY': JSON.stringify(env.VITE_PINATA_API_KEY),
+      'process.env.VITE_PINATA_SECRET_API_KEY': JSON.stringify(env.VITE_PINATA_SECRET_API_KEY)
+    },
+    build: {
+      outDir: 'dist',
+    },
+  }
+})
